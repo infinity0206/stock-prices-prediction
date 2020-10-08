efbedaf6525e9789eace51d273ce9a850841700b
Izumihara Taku <infinity0206@IzumiharanoMacBook-Pro.local>
2020-10-08 23:02:21 +0900

    initial commit

---
+++ data_reader.py
@@ -0,0 +1,10 @@
+import pandas_datareader.data as pdr
+import datetime
+
+end = datetime.date.today()
+start = end - datetime.timedelta(days=10)
+
+pd_data = pdr.DataReader('SNE', 'iex', start, end)
+
+print(pd_data)
+pd_data.to_csv("/app/data.csv")
---
+++ self-regression.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+data = pd.read_csv('/app/data.csv')
+
+print(data["date"])
