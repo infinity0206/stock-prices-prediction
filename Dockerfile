65832cd74f4be2619839915aa3a99481165ec42f
izumihara <infinity0206.tab@gmail.com>
2020-11-18 18:27:11 +0900

    結局sarimaやめて、別の3モデルを導入した

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,9 @@ RUN pip install pandas
 RUN pip install pandas_datareader
 RUN pip install statsmodels
 RUN pip install matplotlib
+RUN pip install u8darts
+RUN pip install darts
+RUN pip install backtesting
+RUN pip install fbprophet
+
+WORKDIR /app
--- prediction.py
+++ prediction.py
@@ -1,60 +1,37 @@
-import warnings
-warnings.filterwarnings('ignore') # 計算警告を非表示
-warnings.simplefilter('ignore')
-
 import pandas as pd
-import numpy as np
-from statsmodels.tsa.arima_model import ARIMA
-import statsmodels.api as sm
+
+df = pd.read_csv('data/covid-19/pcr_positive_daily.csv', delimiter=",")
+
+from darts import TimeSeries
+series = TimeSeries.from_dataframe(df, time_col='日付', value_cols='PCR 検査陽性者数(単日)')
+
+# ここでtraining setとtest setに分割
+train, val = series.split_after(pd.Timestamp('20200810'))
+
+from darts.models import ExponentialSmoothing
+model = ExponentialSmoothing()
+model.fit(train) 
+prediction = model.predict(len(val))
+
 import matplotlib.pyplot as plt
-import datetime
 
-data = pd.read_csv('/app/data.csv')
-tmd = pd.to_datetime(data['date'])
-data.index = tmd
+series.plot(label='actual', lw=3)
+prediction.plot(label='forecast', lw=3)
+plt.legend()
+plt.xlabel('Year')
 
-columns = "volume"
-predictive_counts = 150
+from darts.models.prophet import Prophet
+models = [ExponentialSmoothing(), Prophet()]
+backtests = [model.backtest(series,
+                            start=pd.Timestamp('20200810'),
+                            forecast_horizon=3)
+             for model in models]
 
+from darts.metrics import mape, rmse
+for i, m in enumerate(models):
+    err = rmse(backtests[i], series)
+    backtests[i].plot(lw=3, label='{}, RMSE={:.2f}'.format(m, err))
+plt.title('Predictive verification of 3 methods')
+plt.legend()
 
-counts = len(data["date"])
-
-pred_start = data.index[0]
-pred_end = data.index[-1]
-focus_start = pred_start
-dynamic_start = data.index[counts - predictive_counts]
-focus_end = pred_end
-focus_time = [pred_start, focus_end]
-
-## open 
-model =  sm.tsa.statespace.SARIMAX(data[columns][:], order=(1, 0, 1), trend='c')
-result = model.fit(disp=False)
-
-# one-step-ahead forecast
-predict = result.get_prediction(start=pred_start, end=pred_end)
-predict_ci = predict.conf_int()
-predict_mean = predict.predicted_mean
-
-# dynamic prediction
-predict_dy = result.get_prediction(start=pred_start, end=pred_end, dynamic = counts - predictive_counts)
-predict_dy_ci = predict_dy.conf_int()
-predict_dy_mean = predict_dy.predicted_mean
-
-fig, ax = plt.subplots(figsize=(12, 6))
-
-# data[columns].plot(color='red', ax=ax)
-data[columns].iloc[:-predictive_counts].plot(color='black', ax=ax)
-data[columns].iloc[-predictive_counts:].plot(color='b', ax=ax, alpha=0.5, style="--")
-
-# one-step-ahead forecast
-predict_mean.plot(color='r',ax=ax, label='One-step-ahead forecast')
-ax.fill_between(predict_ci.index, predict_ci.iloc[:, 0], predict_ci.iloc[:, 1], alpha=0.1)
-
-# dynamic prediction
-predict_dy_mean.plot( color='g',ax=ax, label='dynamic forecast')
-ax.fill_between(predict_dy_ci.index,  predict_dy_ci.iloc[:, 0], predict_dy_ci.iloc[:, 1], alpha=0.1)
-
-ax.set_xlim(focus_time)
-# ax.set_ylim([50, 150])
-
-plt.savefig("result_" + columns + "_" + str(predictive_counts) + ".png", bbox_inches='tight', dpi=120)
+plt.savefig("result2_pcr_positive_daily.png", bbox_inches='tight', dpi=120)
